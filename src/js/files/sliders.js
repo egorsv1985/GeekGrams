a411788c0738416e635f93b324a1ad5788ac2222
egorsv1985 <egorvs2009@gmail.com>
2023-02-20 12:53:10 +0300

    fix build

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -19,7 +19,7 @@ EffectFade, Lazy, Manipulation
 
 // Стили Swiper
 // Базовые стили
-import "../../scss/base/swiper.scss";
+// import "../../scss/base/swiper.scss";
 // Полный набор стилей из scss/libs/swiper.scss
 // import "../../scss/libs/swiper.scss";
 // Полный набор стилей из node_modules
